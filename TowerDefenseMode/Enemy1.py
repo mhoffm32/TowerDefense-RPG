802ccb6e3f2220a34c871dcd41e3fcbe4d27288e
JacobDreyer <jacob.n.dreyer@gmail.com>
2023-03-15 02:52:32 -0400

    Enemy movement in TD mode

--- TowerDefenseMode/Enemy1.py
+++ TowerDefenseMode/Enemy1.py
@@ -1,10 +1,10 @@
-import Enemy
+from TowerDefenseMode.Enemy import Enemy
+
 
 
 class Enemy1(Enemy):
-    def __init__(self, x, y):
-        img_path = ''
-
+    def __init__(self, x, y, img_path):
         super().__init__(x, y, img_path)
+        self.health = 100
 
         #print("initialize Enemy Type 1")
