420da65b9890f736f4f3046af35b89f20c0ed0e3
JacobDreyer <jacob.n.dreyer@gmail.com>
2023-03-17 16:33:42 -0400

    Incorporated Progress bar and added coins&Diamonds

--- World.py
+++ World.py
@@ -1,13 +1,25 @@
 from ExplorationMode.Level import Level
 import TowerDefenseMode.Exterior as Exterior
 import pygame
+import ProgressBar
 
 pygame.init()
 
 screen = pygame.display.set_mode((1000,700))
 screenRectangle = screen.get_rect()
 
-level = Level(screen)
+#Progress Bar Stuff
+progressBar = ProgressBar.ProgressBar(screen)
+pBar_group = pygame.sprite.Group(progressBar)
+level = Level(screen, progressBar)
+
+def render(events):
+    pBar_group.update(events)
+
+    pygame.display.flip()
+    pygame.display.update()
+
+state = 1  # paused is 0
 
 # i am commenting this so it doesnt get in everyones way during testing!
 # uncomment to see the effect by putting a hashtag in front of the triple quotes on both ends.
@@ -70,11 +82,24 @@ for text in text_boxes:
 running = True
 while(running):
 
+    for p in pBar_group:
+        if p.paused:
+            state = 0
+        else:
+            state = 1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    if state == 1:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                for p in pBar_group:
+                    p.add_xp(5)
+
     level.update()
+    pBar_group.update(pygame.event.get())
     pygame.display.flip()
     screen.fill((0,0,0))
 
