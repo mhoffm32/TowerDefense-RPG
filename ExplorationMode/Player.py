4a9b31d6f9d724fcaa38962a11782aa4be7d72b6
JacobDreyer <jacob.n.dreyer@gmail.com>
2023-03-25 22:53:18 -0400

    Full functionality of NPCs

--- ExplorationMode/Player.py
+++ ExplorationMode/Player.py
@@ -163,6 +163,7 @@ class Player(Character):
     def collectItem(self, item):
         self.progressBar.add_coin(item.moneyValue)
         self.progressBar.add_diamond(item.crystalValue)
+        self.progressBar.add_xp(item.xpValue)
 
     #here we can add customizable features with different img urls for 
     #different characters depending on gender, hair color, etc'''
--- ExplorationMode/SubArea/SubArea.py
+++ ExplorationMode/SubArea/SubArea.py
@@ -2,6 +2,7 @@ from ExplorationMode.SubArea.Wall import Wall
 from ExplorationMode.SubArea.Door import Door
 from ExplorationMode.SubArea.Item import Item
 from ExplorationMode.Player import Player
+from ExplorationMode.SubArea.FriendlyCharacter import FriendlyCharacter
 
 import pygame
 
@@ -15,17 +16,20 @@ class SubArea:
     def __init__(self, roomNumber, screen, progressBar, background):
         print("Initialize Sub Area")
         self.roomNumber = roomNumber
+        self.screen = screen
         self.wallGroup = pygame.sprite.Group()
         self.doorGroup = pygame.sprite.Group()
         self.itemGroup = pygame.sprite.Group()
         self.roomItemGroup = pygame.sprite.Group()
-        self.generateItems()
-        self.generateRoom(roomNumber)
-        self.screen = screen
+        self.npcGroup = pygame.sprite.Group()
 
         self.playerGroup = pygame.sprite.Group()
         self.player = Player(0,0,0,0,screen, self, progressBar)
         self.playerGroup.add(self.player)
+
+        self.generateItems()
+        self.generateRoom(roomNumber)
+
 
     def update(self):
         self.screen.blit(self.background, (0, 0))
@@ -33,15 +37,18 @@ class SubArea:
         self.wallGroup.draw(self.screen)
         self.doorGroup.draw(self.screen)
         self.roomItemGroup.draw(self.screen)
+        self.npcGroup.draw(self.screen)
 
         self.player.update()
         self.playerGroup.draw(self.screen)
+        self.npcGroup.update()
 
     def generateRoom(self, roomNumber):
         self.roomNumber = roomNumber
         self.wallGroup.empty()
         self.doorGroup.empty()
         self.roomItemGroup.empty()
+        self.npcGroup.empty()
 
         background_path = backgrounds.get(roomNumber, 'Images/backgrounds/brown_stone_floor.png')
         self.background = pygame.image.load(background_path)
@@ -52,7 +59,9 @@ class SubArea:
             self.wallGroup.add(Wall(0,87,1000,38, 'Images/mapObjects/walls/1000x38_wall.png'))
             self.wallGroup.add(Wall(0, 662, 1000,38, 'Images/mapObjects/walls/1000x38_wall.png'))
 
-            
+
+            npcText = ["Hello Player! welcome to the castle. However there is something you need to know. you know of the tree correct?", "Legend states that long ago there was the ability to destroy it. The power was stored within a powerful crystal however it", "was said to be destroyed many centuries ago. However I believe the fragments still exist and when put back together, will", "have the power to once again destroy the tree. Here, you can have my crystal fragment. I collected this a while ago.",  "Maybe you can recreate the crystal. Although I hope you never need to use it."]
+            self.npcGroup.add(FriendlyCharacter(100,350, self.screen, npcText, self.player, 0, 1, 5))
 
             self.doorGroup.add(Door(350,85,'Images/mapObjects/door_sprite.png', 1, [500,500]))
 
@@ -92,13 +101,19 @@ class SubArea:
                     self.itemGroup.remove(item)
                     self.roomItemGroup.remove(item)
 
-        #Check Collision with Door
-        if(keysPressed[pygame.K_f]):
+
+            #Check Collision with Door
             doorCollisionDictionary = pygame.sprite.groupcollide(self.doorGroup, self.playerGroup, False, False)
             for door in doorCollisionDictionary:
                 self.generateRoom(door.nextLevelNumber)
                 self.player.setPosition(door.newRoomSpawnLocation)
                 break
+
+            npcCollisionDictionary = pygame.sprite.groupcollide(self.npcGroup, self.playerGroup, False, False)
+            for npc in npcCollisionDictionary:
+                npc.interacting = True
+
+        #Check Collision with Door
 
         #Check Collision with Wall
         wallCollisionDictionary = pygame.sprite.groupcollide(self.playerGroup, self.wallGroup, False, False)
