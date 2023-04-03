6972fb65f06619aa5cd5adc4cfc88ce125054126
Isabelle Beaudry Hajji <ibeaudr@uwo.ca>
2023-04-03 19:59:03 -0400

    bug fixing

--- TowerDefenseMode/Enemy1.py
+++ TowerDefenseMode/Enemy1.py
@@ -5,7 +5,7 @@ from TowerDefenseMode.Enemy import Enemy
 class Enemy1(Enemy):
     def __init__(self, x, y, img_path, tower):
         super().__init__(x, y, img_path, tower, 20, 20)
-        self.health = 50
+        self.health = 100
         self.damage = 5
         self.speed = .2
         self.attackSpeed = .5
